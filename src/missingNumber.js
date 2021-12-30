9fcea43f040c64348efbf683a9f7ac28366959e5
Chris-Pagan <christopher.pagan@outlook.com>
2021-12-30 08:08:20 -0800

    all qualified tests passing

--- src/missingNumber.js
+++ src/missingNumber.js
@@ -1,11 +1,27 @@
 /**
  * Implement a brute force algorithm for finding the missing number in an array
  */
-function missingNumberBruteForce(numbers) {}
+function missingNumberBruteForce(numbers) {
+    let currentNum = 1;
+    let missingNum = 0;
+    while (missingNum === 0){
+        if (numbers.includes(currentNum)){
+            currentNum++
+        } else {
+            missingNum = currentNum;
+        }
+    }
+    return missingNum;
+}
 
 /**
  * Use an iterative  strategy for finding the missing number in an array
  */
-function missingNumberSum(numbers) {}
+function missingNumberSum(numbers) {
+    const nums = numbers.length + 1;
+    let sum = 0;
+    numbers.forEach((number) => sum += number)
+    return (nums * (nums + 1)) /2 - sum;
+}
 
 module.exports = { missingNumberBruteForce, missingNumberSum };
